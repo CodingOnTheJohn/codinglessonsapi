bc16ac83b5f1a5f220c8c9a6d501adcd0a356fe6
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-23 11:53:27 -0500

    Feat: DB Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      
+      resources :ruby, only: [:index, :show]
+      resources :javascript, only: [:index, :show]
     end
   end
 end
