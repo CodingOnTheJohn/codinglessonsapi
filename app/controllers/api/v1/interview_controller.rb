c8a50604cdc505da01a94d04492051769b4b8e2f
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-23 18:11:53 -0500

    Feat: Interview Endpoints tested

--- app/controllers/api/v1/interview_controller.rb
+++ app/controllers/api/v1/interview_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::InterviewController < ApplicationController
   def index
-    @interviews = Interview.all
+    @interviews = InterviewQuestion.all
     render json: InterviewSerializer.new(@interviews)
   end
 
   def show
-    @interview = Interview.find(params[:id])
+    @interview = InterviewQuestion.find(params[:id])
     render json: InterviewSerializer.new(@interview)
   end
 end
