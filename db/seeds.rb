6ff837210907b290d415257067e378f1de985b48
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-08-19 14:38:25 -0600

    basic markdown updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,40 @@
 # Create Interview Lessons
 Lesson.create(
   lesson_type: :interview,
-  lesson: "Explain the following Data Types: String, Integer, Float, Boolean, Array, Hash",
+  lesson: "Explain the following Data Types: `String, Integer, Float, Boolean, Array, Hash`",
   topic: "Data Types",
   link: "https://backend.turing.edu/module1/lessons/datatypes"
 )
 Lesson.create(
   lesson_type: :interview,
-  lesson: "What is the differnce between a .map and a .each method in Ruby?",
+  lesson: "What is the differnce between a `.map` and a `.each` method in Ruby?",
   topic: "Enumerables",
   link: "https://medium.com/@abhishektanwar9596/what-is-the-difference-between-map-and-each-in-ruby-1f2fbb5d5842"
 )
 Lesson.create(
   lesson_type: :interview,
   lesson: "What is polymorphism in Object Oriented Programming?",
-  topic: "OOP",
+  topic: "Polymorphism",
   link: "https://medium.com/@shanikae/polymorphism-explained-simply-7294c8deeef7"
 )
 
 # Create JavaScript Lessons
 Lesson.create(
   lesson_type: :javascript,
-  lesson: "Objects:
-  Objects are used to store collections of data and more complex entities. They are created using curly braces {} and can contain properties.
+  lesson: "Objects are used to store collections of data and more complex entities. They are created using curly braces {} and can contain properties.
+  ```
   let person = {
     name: 'Alice',
     age: 30,
     height: 5.6
     };
+    ```
     The person object has three properties: name, age, and height. Properties can be accessed using dot notation or bracket notation.
-    console.log(person.name); // 'Alice'
-    console.log(person['age']); // 30
+    `console.log(person.name); // 'Alice'`
+    `console.log(person['age']); // 30`
     To add or update a property, you can simply assign a value to it.
-    person.city = 'New York'; // Add a new property
-    person.age = 31; // Update an existing property",
+    `person.city = 'New York'; // Add a new property`
+    `person.age = 31; // Update an existing property`",
   topic: "Objects",
   link: "https://www.example.com/javascript_lessons/1"
 )
@@ -52,8 +53,8 @@ Lesson.create(
 )
 Lesson.create(
   lesson_type: :javascript,
-  lesson: "Conditional Statements:
-  Conditional statements allow us to execute different code based on different conditions.
+  lesson: "Conditional statements allow us to execute different code based on different conditions.
+  ```
   let number = 10;
 
   if (number > 5) {
@@ -63,17 +64,17 @@ Lesson.create(
   } else {
     console.log('The number is less than 5.');
   }
+  ```
   Conditions are checked in order, and the first true condition executes its block.
-  === checks for strict equality, > and < for greater or lesser comparisons.",
-  topic: "Conditional Logic",
+  `===` checks for strict equality, `>` and `<` for greater or lesser comparisons.",
+  topic: "Conditional Statements",
   link: "https://www.example.com/javascript_lessons/1"
 )
 
 # Create Ruby Lessons
 Lesson.create(
   lesson_type: :ruby,
-  lesson: "## Arrays:
-  Arrays are ordered lists that are comma seperated and enclosed in square brackets. Arrays can contain different data types (integer, string, hash, array). You can mix data types in arrays but it is not recommended.
+  lesson: "Arrays are ordered lists that are comma seperated and enclosed in square brackets. Arrays can contain different data types (integer, string, hash, array). You can mix data types in arrays but it is not recommended.
   
   `fruits = ['apple', 'banana', 'cherry']`
 
@@ -89,14 +90,14 @@ Lesson.create(
   `fruits << 'date'`
   
   `puts fruits # Outputs ['apple', 'banana', 'cherry', 'date']`",
-  topic: "Data Types",
+  topic: "Arrays",
   link: "https://ruby-doc.org/core-2.7.0/Array.html"
 )
 Lesson.create(
   lesson_type: :ruby,
-  lesson: "Conditional Statements:
-  Conditional statements allow us to execute different code based on different conditions.
+  lesson: "Conditional statements allow us to execute different code based on different conditions.
   example:
+  ```
   number = 5
   if number > 5
     puts 'The number is greater than 5.'
@@ -105,27 +106,30 @@ Lesson.create(
   else
     puts 'The number is less than 5.'
   end
+  ```
   Conditions are checked in order, and the first true condition executes its block.",
-  topic: "Conditional Logic",
+  topic: "Conditional Statements",
   link: "https://www.rubyguides.com/ruby-tutorial/ruby-if-else/"
 )
 Lesson.create(
   lesson_type: :ruby,
-  lesson: ".each method:
-  The .each method is an enumerable method that allows you to iterate over each element in an array or hash.
+  lesson: "The `.each` method is an enumerable method that allows you to iterate over each element in an array or hash.
   example:
+  ```
   fruits = ['apple', 'banana', 'cherry']
   fruits.each do |fruit|
     puts fruit
   end
-  the line 'puts fruit' will be executed for each element in the array, the |fruit| is a block variable that represents the current element in the iteration.
-  
+  ```
+  the line `puts fruit` will be executed for each element in the array, the `|fruit|` is a block variable that represents the current element in the iteration.
+  ```
   hash = {name: 'Alice', age: 30}
   hash.each do |key, value|
     puts key
     puts value
   end
-  the line 'puts key' will output the key and 'puts value' will output the value for each key-value pair in the hash.",
+  ```
+  the line `puts key` will output the key and `puts value` will output the value for each key-value pair in the hash.",
   topic: "iterating",
   link: "https://ruby-doc.org/core-2.7.0/Enumerable.html#method-i-each"
 )
